718523305819974d33a323ba3e7a5d0bb3653b7a
Leoshirei <Leoshirei@DESKTOP-505B8AM>
2026-08-29 11:47:00 +0200

    feat: create update class feat: add update method

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include "Window.h"
+#include "Update.h"
 
 
 int main()
 {
 	Window window;
+	Update update;
 	while (window.isOpen())
 	{
-		
+		update.update(window);
 	}
 }
